1e35037047db63f1488284e6924cab729aab5e03
Sheha89 <shehan.ab@gmail.com>
2019-04-06 11:00:20 +0800

    Skill entity added

---
+++ src/main/java/com/skilltracker/model/Skill.java
@@ -0,0 +1,28 @@
+package com.skilltracker.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Builder
+@Table(name = "skill")
+public class Skill {
+
+
+    private int id;
+
+    private int skillTypeId;
+
+    private String skillTitle;
+
+
+}
