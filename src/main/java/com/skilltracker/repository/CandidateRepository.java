2c6df48e0a3e8f1c206ec6f2a493a7c51f2ccc13
buddhi <buddhi.1986@gmail.com>
2019-04-06 11:43:14 +0800

    maria db driver

--- src/main/java/com/skilltracker/repository/CandidateRepository.java
+++ src/main/java/com/skilltracker/repository/CandidateRepository.java
@@ -1,9 +1,9 @@
 package com.skilltracker.repository;
 
-import com.skilltracker.model.User;
+import com.skilltracker.model.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface CandidateRepository extends JpaRepository<Candidate, Long> {
 }
