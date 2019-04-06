1c80cfd7478dee706369f1aa8c8f4131f6f5649c
Buddhi <buddhi@Buddhis-MacBook-Pro.local>
2019-04-06 10:46:46 +0800

    controller

--- src/main/java/com/skilltracker/controller/SkillTrackerController.java
+++ src/main/java/com/skilltracker/controller/SkillTrackerController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ReMapping(path = "/skills")
 public class SkillTrackerController {
-    @GetMapping(path = "")
 }
--- src/main/java/com/skilltracker/repository/SkillTrackerRepository.java
+++ src/main/java/com/skilltracker/repository/SkillTrackerRepository.java
@@ -2,6 +2,8 @@ package com.skilltracker.repository;
 
 import com.skilltracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface SkillTrackerRepository extends JpaRepository<User, Long> { 
+@Repository
+public interface SkillTrackerRepository extends JpaRepository<User, Long> {
 }
